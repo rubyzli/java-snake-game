e9d3d68d8131419dfcfda47861378d8d6cb5809b
Jessica <jr.simon07@gmail.com>
2023-03-16 15:11:31 +0100

    GamePanel methods set up

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GamePanel extends JPanel implements ActionListener {
     GamePanel() {
@@ -10,11 +13,38 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
-    public void paintComponent(){
-        
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public  void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+         @Override
+        public void keyPressed(KeyEvent e){
+
+         }
     }
 }
