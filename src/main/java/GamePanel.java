2eb0a121435e2859927f4e21e7dd63131f859890
Jessica <jr.simon07@gmail.com>
2023-03-16 15:05:33 +0100

    Initial commit

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+    GamePanel() {
+
+    }
+    public void startGame(){
+
+    }
+
+    public void paintComponent(){
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
